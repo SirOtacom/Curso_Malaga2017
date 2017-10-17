be912bb9a3d46ce1f6e5504e76f13b9ad5925343
SirOtacom <malaga_main@hotmail.com>
2017-10-17 12:43:27 +0200

    modulos

---
+++ 02_modulos/modulos/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PieComponent } from './pie/pie.component';
+import { CabezaComponent } from './cabeza/cabeza.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PieComponent,
+    CabezaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
